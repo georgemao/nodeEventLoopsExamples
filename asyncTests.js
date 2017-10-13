f6071925f7300cce8d6a0de4a9e7abf9d3a927a6
georgmao <georgmao@amazon.com>
2017-10-13 11:19:01 -0700

    first commit

---
+++ asyncTests.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+var async = require("async");
+
+exports.handler = (event, context, callback) => {
+
+	// Perform these async calls in series
+	async.series([
+		function(callback){
+			console.log('ji');
+			callback(null, 'one');
+		},
+		function(callback){
+			console.log('adsf');
+			callback(null, 'two');
+		}
+	],
+	function(err, results){
+		console.log(results);
+		
+	});
+
+	// Waterfall the result of each function to the next
+	async.waterfall([
+		function(callback){
+			callback(null, 'one');
+		},
+		function(arg1, callback){
+			console.log(arg1);
+			callback(null, 'two');
+		}
+	], function(err, result){
+		console.log(result);
+		callback(null, {statusCode: 200})
+	});
+}
